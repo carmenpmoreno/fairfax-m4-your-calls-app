2b863d37ce154cf91836ff09bba9cdacc2bad201
carmenpmoreno <carmenp.moreno@gmail.com>
2019-06-03 14:05:55 +0200

    first try to paint companies

--- src/components/CompanyFilter.js
+++ src/components/CompanyFilter.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
 
 class CompanyFilter extends Component {
+
+
     render() {
         return(
             <fieldset>
                 <legend>Filtro por cliente</legend>
                 <label>Cliente</label>
                 <select id="company" name="company">
-                    <option value=""></option>
+                {fetch('https://adalab.interacso.com/api/graph/bar')
+                    .then(response => response.json())
+                    .then(data => {
+                        const keys = Object.keys(data[0]);
+                    return (
+                        {keys.map (item => {
+                            <option value={item}></option>
+                        })}
+                    );
+                    })
+                }
                 </select>
-
             </fieldset>
         );
     }
