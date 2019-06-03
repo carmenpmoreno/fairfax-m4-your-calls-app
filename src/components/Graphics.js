a79f4f43ebcb79facfaed6939d910c60d749f387
carmen-tm <carmentorremolina@gmail.com>
2019-06-03 15:34:13 +0200

    Add chart bar with fake provisional data

--- src/components/Graphics.js
+++ src/components/Graphics.js
@@ -1,12 +1,83 @@
-import React, {Component} from 'react';
+import React, { Component, Fragment } from 'react';
+import Chart from 'react-google-charts';
 
+//Fake data from Mock
+const chartDataBars = [
+  ['Meses', 'ikea', 'racc', 'cc', 'audi', 'tork'],
+  ['Enero', 1, 5, 3, 3, 1],
+  ['Febrero', 2, 15, 6, 3, 2],
+  ['Marzo', 2, 8, 9, 2, 3],
+  ['Abril', 2, 4, 3, 1, 2]
+];
 
 class Graphics extends Component {
-    render () {
-        return (
-            'Soy Graphics y aqui van los componentes Chart'
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        return this.state.barsLoading === 'ready' ? (
+        <Chart
+          chartType="BarChart"
+          loader={<div>Loading Chart</div>}
+          width={'800px'}
+          height={'300px'}
+          // data={this.state.chartDataBars}
+          data={chartDataBars}
+          options={{
+            title: 'Llamadas totales',
+            chartArea: {
+              width: '50%',
+              stroke: '#1C1C1C'
+            },
+            isStacked: true,
+            orientation: 'horizontal',
+
+            hAxis: {
+              title: 'Mes',
+              titleTextStyle: {
+                color: '#fff'
+              },
+              textStyle: {
+                color: '#fff'
+              },
+              minValue: 0
+            },
+
+            vAxis: {
+              title: 'Número de llamadas',
+              titleTextStyle: {
+                color: '#fff'
+              }
+            },
+
+            legend: {
+              //To place the legend on the top
+              position: 'top',
+              alignment: 'start',
+
+              //Working
+              textStyle: {
+                color: '#fff',
+                fontSize: 14,
+                fontName: 'Arial',
+                bold: false,
+                italic: false
+              }
+            },
+
+            backgroundColor: '#1C1C1C',
+            titleTextStyle: {
+              color: 'white',
+              fontSize: 16,
+              fontName: 'Arial',
+              bold: true
+            }
+          }}
+        />
+        ) : (<div>Fetching data from API</div>
+        );
+      </Fragment>
+    );
+  }
 }
 
 export default Graphics;
