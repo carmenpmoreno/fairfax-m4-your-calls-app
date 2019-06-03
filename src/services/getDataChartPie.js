ddb3e6f7a0c148176285df17c736b0d6ca92d1ff
carmen-tm <carmentorremolina@gmail.com>
2019-06-03 13:21:48 +0200

    Add logic for deafult range

--- src/services/getDataChartPie.js
+++ src/services/getDataChartPie.js
@@ -1,22 +1,42 @@
 //Graph api call by default (1month)
-const ENDPOINT = 'https://adalab.interacso.com/api/graph/pie';
+let actualDate = '';
+let actualMonthRange = '';
 
-
-//First: know the actual date
-currentTime() {
+// First: know the actual date
+const currentTime = () => {
   let date = new Date();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
+  let day = date.getDay();
+  let month = date.getMonth();
+  let year = date.getYear();
 
-  let defaultDate = `${hours}:${minutes}:${seconds}`;
+  actualDate = `${day}/${month}/${year}`;
+  actualMonthRange = `${day}/${month - 1}/${year}`;
+};
 
-}
-curentTime();
-console.log(date);
+currentTime();
+console.log(actualDate, actualMonthRange);
 
+//Goal
+// actualDate = '03/06/2019';
+// actualMonthRange = '03/05/2019';
 
-// //Second: calculate last month range
+const URL = `https://adalab.interacso.com/api/graph/pie?from=${actualMonthRange}&to=${actualDate}`;
 
-// fetch('https://private-afa254-adalabapi.apiary-mock.com/pie?from=18/01/2019&to=01/02/2019&client=ikea')
-// .then(response=> response.json())
+const fetchChartPie = () => {
+  return fetch(URL)
+    .then(response => response.json())
+    .then(data => console.log(data));
+};
+
+fetchChartPie();
+
+//Expected data
+// {
+//   "neutral": 38,
+//   "bad": 28,
+//   "good": 31,
+//   "undefined": 10,
+//   "": 2
+// }
+
+export { fetchChartPie };
