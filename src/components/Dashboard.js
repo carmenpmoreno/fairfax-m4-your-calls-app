7fa3b62cd83cc1e33ad9f49ccf949058892c46c6
valerialp <valeria.lorentep@gmail.com>
2019-06-04 10:57:54 +0200

    Add data back in dashborad

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,15 +4,53 @@ import Filters from "./Filters";
 import Graphics from "./Graphics";
 
 class Dashboard extends Component {
-    render () {
-        return (
-            <div>
-                <Filters 
-                    actionSetFilterStartDate ={this.props.actionSetFilterStartDate}
-                    actionSetFilterEndDate ={this.props.actionSetFilterEndDate}
-                    dateStart={this.props.dateStart}
-                    dateEnd={this.props.dateEnd}
-                />
+  render() {
+    const dataOriginal = [
+      {
+        month: 0,
+        year: 2018,
+        ikea: 1,
+        racc: 5,
+        cc: 3,
+        audi: 3,
+        tork: 1
+      },
+      {
+        month: 1,
+        year: 2018,
+        ikea: 2,
+        racc: 15,
+        cc: 6,
+        audi: 3,
+        tork: 2
+      },
+      {
+        month: 2,
+        year: 2018,
+        ikea: 2,
+        racc: 8,
+        cc: 9,
+        audi: 2,
+        tork: 3
+      },
+      {
+        month: 3,
+        year: 2018,
+        ikea: 2,
+        racc: 4,
+        cc: 3,
+        audi: 1,
+        tork: 2
+      }
+    ];
+    return (
+      <div>
+        <Filters
+          actionSetFilterStartDate={this.props.actionSetFilterStartDate}
+          actionSetFilterEndDate={this.props.actionSetFilterEndDate}
+          dateStart={this.props.dateStart}
+          dateEnd={this.props.dateEnd}
+        />
 
         <Graphics
           pieData={this.props.pieData}
@@ -24,12 +62,12 @@ class Dashboard extends Component {
 }
 
 Dashboard.propTypes = {
-    pieData: PropTypes.arrayOf(PropTypes.array),
-    pieLoading: PropTypes.string.isRequired,
-    actionSetFilterStartDate: PropTypes.func.isRequired,
-    actionSetFilterEndDate: PropTypes.func.isRequired,
-    dateStart: PropTypes.string.isRequired,
-    dateEnd: PropTypes.string.isRequired,
+  pieData: PropTypes.arrayOf(PropTypes.array),
+  pieLoading: PropTypes.string.isRequired,
+  actionSetFilterStartDate: PropTypes.func.isRequired,
+  actionSetFilterEndDate: PropTypes.func.isRequired,
+  dateStart: PropTypes.string.isRequired,
+  dateEnd: PropTypes.string.isRequired
 };
 
 export default Dashboard;
