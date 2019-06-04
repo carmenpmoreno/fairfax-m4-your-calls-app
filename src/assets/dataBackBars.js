be9e6e50af42a44160a29b1da327283395ef4e65
valerialp <valeria.lorentep@gmail.com>
2019-06-04 11:53:14 +0200

    Add assets

---
+++ src/assets/dataBackBars.js
@@ -0,0 +1,41 @@
+
+const dataBackBars = [
+	{
+		month: 2,
+		year: 2018,
+		ikea: 1,
+		racc: 5,
+		cc: 3,
+		audi: 3,
+		tork: 1
+	},
+	{
+		month: 1,
+		year: 2018,
+		ikea: 2,
+		racc: 15,
+		cc: 6,
+		audi: 3,
+		tork: 2
+	},
+	{
+		month: 2,
+		year: 2018,
+		ikea: 2,
+		racc: 8,
+		cc: 9,
+		audi: 2,
+		tork: 3
+	},
+	{
+		month: 3,
+		year: 2018,
+		ikea: 2,
+		racc: 4,
+		cc: 3,
+		audi: 1,
+		tork: 2
+	}
+];
+
+export default dataBackBars;
