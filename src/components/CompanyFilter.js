b60961addd72e62342b282d94d96f81c2967c60f
carmenpmoreno <carmenp.moreno@gmail.com>
2019-06-05 11:53:35 +0200

    add final styles

--- src/components/CompanyFilter.js
+++ src/components/CompanyFilter.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/CompanyFilter.scss';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+library.add(faChevronDown);
 
 class CompanyFilter extends Component {
   render() {
@@ -10,19 +14,25 @@ class CompanyFilter extends Component {
       <fieldset className="companyFilterContainer">
         <legend className="companyFilterLegend">Cliente</legend>
         <label className="companyFilterLabel">Filtro por cliente</label>
-        <select className="companySelect" id="company" name="company" value={companySelected} onChange={getCompanySelected}>
-          <option value="">Elige una compañia</option>
-          {allCompanies.map((item, index) => {
-            return (
-              <option
-                key={index + 1}
-                value={item}
-              >
-                {item}
-              </option>
-            );
-          })}
-        </select>
+        <div className="selectContainer">
+          <select
+            className="companySelect"
+            id="company"
+            name="company"
+            value={companySelected}
+            onChange={getCompanySelected}
+          >
+            <option value="">Elige una compañia</option>
+            {allCompanies.map((item, index) => {
+              return (
+                <option key={index + 1} value={item}>
+                  {item}
+                </option>
+              );
+            })}
+          </select>
+          <FontAwesomeIcon icon="chevron-down" className="faChevronDown" />
+        </div>
       </fieldset>
     );
   }
@@ -31,7 +41,7 @@ class CompanyFilter extends Component {
 CompanyFilter.propTypes = {
   allCompanies: PropTypes.arrayOf(PropTypes.string),
   getCompanySelecte: PropTypes.func,
-  companySelected: PropTypes.string,
+  companySelected: PropTypes.string
 };
 
 export default CompanyFilter;
--- src/components/DateFilter.js
+++ src/components/DateFilter.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/DateFilter.scss';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+library.add(faChevronDown);
 
 class DateFilter extends Component {
   render() {
