e6ec4ca920ca14dbf55be1e10429019804176237
sandrapinazo <sandrapinazogonzalez@gmail.com>
2019-06-05 11:26:17 +0200

    Fix default dates

--- src/components/DateFilter.js
+++ src/components/DateFilter.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "react-dates/initialize";
+
+import { DateRangePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
 
 class DateFilter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      focusedInput: null,
+    };
+  }
   render() {
     return (
       <div>
         <h2>Rango de fecha</h2>
+        <div className="App">
+          <DateRangePicker
+            startDateId="startDate"
+            endDateId="endDate"
+            startDate={this.props.dateStart}
+            endDate={this.props.dateEnd}
+            onDatesChange={({ startDate, endDate }) => {
+              this.setState({ startDate, endDate });
+            }}
+            focusedInput={this.state.focusedInput}
+            onFocusChange={focusedInput => {
+              this.setState({ focusedInput });
+            }}
+          />
+        </div>
         <div className="main__subtitle--date-container">
           <div className="dateStart">
             <label htmlFor="dateStart" />
